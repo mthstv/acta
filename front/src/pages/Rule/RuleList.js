2ed01a9ecb566559efcb783e4f2999620d2d4cd4
matheust45 <matheus.tavares45@gmail.com>
2020-02-25 21:50:58 -0300

    deleting rules

--- front/src/pages/Rule/RuleList.js
+++ front/src/pages/Rule/RuleList.js
@@ -1,9 +1,14 @@
-import React from "react";
-import RuleCard from "../../components/RuleCard";
+import React, { Component } from "react";
+import RuleCard from "./components/RuleCard";
 import { Row, Col } from "react-bootstrap";
 import api from "../../services/api";
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
+
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import * as snackbarActions from '../../_actions/snackbar'
 
 const styles = {
   fab: {
@@ -15,11 +20,15 @@ const styles = {
     position: 'fixed',
   }
 };
-class RuleList extends React.Component {
+class RuleList extends Component {
   constructor(props) {
     super(props);
     this.state = {};
 
+    this.getRules()
+  }
+
+  getRules = () => {
     api.get('/rule')
     .then((res) => {
       this.setState({ rules: res.data.data });
@@ -28,6 +37,20 @@ class RuleList extends React.Component {
       if(err.response.status === 401) {
         window.location.href = '/login'
       }
+    })
+  }
+
+  /**
+   * Receives roleID from child to delete 
+   */
+  handleDelete = async (ruleID) => {
+   api.delete(`/rule/${ruleID}`)
+    .then((res) => {
+      this.props.snackbarActions.showSnackbar('Regra excluída com sucesso')
+      this.getRules()
+    })
+    .catch((err) => {
+      this.props.snackbarActions.showSnackbar('Houve um problema ao realizar esta ação')
     })
   }
 
@@ -45,10 +68,16 @@ class RuleList extends React.Component {
                   title={ item.rule_title }
                   text={ item.description }
                   ruleId={item.id}
+                  handleDelete={this.handleDelete}
                 />
               </Col>
             </Row>
-          )}
+            )}
+          {rules && rules.length === 0 ?
+            <div style={{textAlign: 'center', color: 'white'}}>
+              Nenhuma regra encontrada
+            </div>
+          :''}
         </div>
         <Fab 
           color="primary" 
@@ -63,4 +92,11 @@ class RuleList extends React.Component {
   }
 }
 
-export default RuleList;
+function mapDispatchToProps (dispatch) {
+  return {
+    snackbarActions: bindActionCreators(snackbarActions, dispatch),
+  }
+}
+
+
+export default connect(null, mapDispatchToProps)(RuleList);
