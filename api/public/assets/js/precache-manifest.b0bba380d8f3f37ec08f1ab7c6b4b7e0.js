b9a1a040e631d109fd2fa0521686128a42c3bfff
matheust45 <matheus.tavares45@gmail.com>
2020-02-26 18:37:44 -0300

    adding npm run build on api public

---
+++ api/public/assets/js/precache-manifest.b0bba380d8f3f37ec08f1ab7c6b4b7e0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b149e4d875fb06e4cdd89a703c5a5b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b3ccb8c523301a896a8",
+    "url": "/static/css/2.9856cb51.chunk.css"
+  },
+  {
+    "revision": "7bd46e397c9e10bb4edc",
+    "url": "/static/css/main.1bfae844.chunk.css"
+  },
+  {
+    "revision": "8b3ccb8c523301a896a8",
+    "url": "/static/js/2.ef305aec.chunk.js"
+  },
+  {
+    "revision": "146ae3b266de6b136141c63f2aacd45b",
+    "url": "/static/js/2.ef305aec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd46e397c9e10bb4edc",
+    "url": "/static/js/main.ae8fcea3.chunk.js"
+  },
+  {
+    "revision": "315ab9ca36525bad744b",
+    "url": "/static/js/runtime-main.46e3b13c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
