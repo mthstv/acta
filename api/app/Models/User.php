d829babbba07b448968987d5e3f8a58a67a5017b
matheust45 <matheus.tavares45@gmail.com>
2020-06-02 09:29:52 -0300

    user auth token adjustment

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -34,7 +34,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token', 'auth_token'
+        'password', 'remember_token'
     ];
 
     /**
