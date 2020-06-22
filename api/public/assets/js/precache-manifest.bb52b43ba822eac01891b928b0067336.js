f4d90587559f6800b12f4ab08c0ab34cc94012a2
matheust45 <matheus.tavares45@gmail.com>
2020-06-22 10:52:49 -0300

    v2.0

---
+++ api/public/assets/js/precache-manifest.bb52b43ba822eac01891b928b0067336.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "915a96fd2c4d7450e0bf06a9ea1bced3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9338724e0e24ebe9a4f3",
+    "url": "/static/css/2.9856cb51.chunk.css"
+  },
+  {
+    "revision": "43c04d403e60bb2f63fd",
+    "url": "/static/css/main.cbf8dd2d.chunk.css"
+  },
+  {
+    "revision": "9338724e0e24ebe9a4f3",
+    "url": "/static/js/2.6db4df52.chunk.js"
+  },
+  {
+    "revision": "17f488f785ccdad34ba00d7581edaf3b",
+    "url": "/static/js/2.6db4df52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43c04d403e60bb2f63fd",
+    "url": "/static/js/main.41315a63.chunk.js"
+  },
+  {
+    "revision": "8a21a9610ca2905bab57",
+    "url": "/static/js/runtime-main.89f24a37.js"
+  },
+  {
+    "revision": "bb1a702466961e0a111248635278e358",
+    "url": "/static/media/book_shelf.bb1a7024.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "57103ac6fe78da2014764d39f207ac0a",
+    "url": "/static/media/search-by-algolia.57103ac6.svg"
+  },
+  {
+    "revision": "95f81f823adb4fe596de1cdfb463b9e5",
+    "url": "/static/media/user-profile.95f81f82.png"
+  }
+]);
--- api/public/assets/js/service-worker.js
+++ api/public/assets/js/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b5ad2f71279724a8ca4f32714502112d.js"
+  "/precache-manifest.bb52b43ba822eac01891b928b0067336.js"
 );
 
 self.addEventListener('message', (event) => {
